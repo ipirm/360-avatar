151102f43cdc758fa7cb66a1a08b01fcbb433b4e
unknown <ilham>
2021-05-09 11:44:14 +0400

    First

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-three-sixty
+        :amount=149
+        imagePath="/assets"
+        fileName="{index}.png"
+    />
+    <vue-three-sixty
+        :amount=149
+        imagePath="/assets"
+        fileName="{index}.png"
+    />
+    <vue-three-sixty
+        :amount=149
+        imagePath="/assets"
+        fileName="{index}.png"
+    />
+    <vue-three-sixty
+        :amount=149
+        imagePath="/assets"
+        fileName="{index}.png"
+    />
+    <vue-three-sixty
+        :amount=149
+        imagePath="/assets"
+        fileName="{index}.png"
+    />
+    <vue-three-sixty
+        :amount=149
+        imagePath="/assets"
+        fileName="{index}.png"
+    />
+    <vue-three-sixty
+        :amount=149
+        imagePath="/assets"
+        fileName="{index}.png"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
@@ -24,5 +56,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueThreeSixty from 'vue-360'
+
+import 'vue-360/dist/css/style.css'
+
+Vue.use(VueThreeSixty)
+
 Vue.config.productionTip = false
 
 new Vue({
